bf42978339a602c01f8c074c1b1d494604b1e265
zhubochao <1060007045@qq.com>
2022-05-28 23:31:43 +0800

    src

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <template>
   <div>
+    <GIF :src="src" />
     <hr />
-    <img
-      :src="src"
-      :rel:animated_src="src"
-      width="360"
-      height="360"
-      rel:auto_play="1"
-      rel:rubbable="1"
-      ref="container"
-    />
   </div>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
-import libgif from '@/lib/libgif/libgif'
-import coug3 from '@/assets/coug3.gif'
-const SRCS = Object.freeze([
-  coug3,
-  'https://img.zcool.cn/community/01584659ccc891a801218e18e4097e.gif',
-  'https://img.zcool.cn/community/0197d259ccc891a8012053f8cb26e3.gif'
-] as const)
-const container = ref<null | HTMLImageElement>(null)
+import GIF from '@/components/Gif.vue'
+import { SRCS } from './metaData';
 const src = SRCS[0]
-let rub: any
-onMounted(async () => {
-  if (container.value instanceof HTMLImageElement) {
-    rub = libgif({ gif: container.value })
-    rub.load()
-  }
-})
 </script>
---
+++ src/metaData.ts
@@ -0,0 +1,6 @@
+import coug3 from '@/assets/coug3.gif'
+export const SRCS = [
+  coug3,
+  'https://img.zcool.cn/community/01584659ccc891a801218e18e4097e.gif',
+  'https://img.zcool.cn/community/0197d259ccc891a8012053f8cb26e3.gif'
+] as const
