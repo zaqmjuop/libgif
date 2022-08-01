3dc1f605ab0f20cbeb3eab32fe32dd6d07c087f8
朱博超 <1060007045@qq.com>
2022-08-02 03:50:41 +0800

    修改变量名

--- src/lib/supergif/viewer.ts
+++ src/lib/supergif/viewer.ts
@@ -3,14 +3,14 @@ export class Viewer {
   $el?: HTMLImageElement
   readonly canvas = document.createElement('canvas') // 缩放滤镜后的模样
   readonly ctx: CanvasRenderingContext2D
-  readonly utilCanvas = document.createElement('canvas') // 图片文件原始模样
-  readonly utilCtx: CanvasRenderingContext2D
+  readonly draftCanvas = document.createElement('canvas') // 图片文件原始模样
+  readonly draftCtx: CanvasRenderingContext2D
   opacity = 255
   zoomW = 1
   zoomH = 1
   constructor() {
     this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D
-    this.utilCtx = this.utilCanvas.getContext('2d') as CanvasRenderingContext2D
+    this.draftCtx = this.draftCanvas.getContext('2d') as CanvasRenderingContext2D
   }
   get showProgressBar() {
     return this.$el?.getAttribute('progress_bar') !== 'none'
@@ -63,11 +63,11 @@ export class Viewer {
     this.ctx.scale(zoomW, zoomH)
     this.zoomW = zoomW
     this.zoomH = zoomH
-    this.utilCanvas.width = imgSize.width
-    this.utilCanvas.height = imgSize.height
-    this.utilCanvas.style.width = imgSize.width + 'px'
-    this.utilCanvas.style.height = imgSize.height + 'px'
-    this.utilCtx.setTransform(1, 0, 0, 1, 0, 0)
+    this.draftCanvas.width = imgSize.width
+    this.draftCanvas.height = imgSize.height
+    this.draftCanvas.style.width = imgSize.width + 'px'
+    this.draftCanvas.style.height = imgSize.height + 'px'
+    this.draftCtx.setTransform(1, 0, 0, 1, 0, 0)
   }
   drawProgress(percent: number) {
     if (percent > 1 || percent < 0 || !this.showProgressBar) return
@@ -100,7 +100,7 @@ export class Viewer {
     this.ctx.stroke()
   }
   getDraft(rect: Rect) {
-    return this.utilCtx.getImageData(
+    return this.draftCtx.getImageData(
       rect.leftPos,
       rect.topPos,
       rect.width,
@@ -112,17 +112,17 @@ export class Viewer {
     left: number = 0,
     top: number = 0
   ) {
-    const { width, height } = this.utilCanvas
+    const { width, height } = this.draftCanvas
     if (!picture) {
-      this.utilCtx.clearRect(0, 0, width, height)
+      this.draftCtx.clearRect(0, 0, width, height)
     } else if (picture instanceof ImageData) {
-      this.utilCtx.putImageData(picture, left, top)
+      this.draftCtx.putImageData(picture, left, top)
     } else {
-      this.utilCtx.fillStyle = `rgb(${picture.join(',')} )`
-      this.utilCtx.fillRect(0, 0, width, height)
+      this.draftCtx.fillStyle = `rgb(${picture.join(',')} )`
+      this.draftCtx.fillRect(0, 0, width, height)
     }
   }
   drawDraft() {
-    this.ctx.drawImage(this.utilCanvas, 0, 0) // 真正的视图画布
+    this.ctx.drawImage(this.draftCanvas, 0, 0) // 真正的视图画布
   }
 }
