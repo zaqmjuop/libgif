7bc14d919188d2504060d893a8675db4ffe38408
朱博超 <1060007045@qq.com>
2022-08-07 02:33:46 +0800

    删除

--- src/utils/date.ts
+++
@@ -1,4 +0,0 @@
-import dayjs from 'dayjs'
-
-export const toLocaleString = (msec: number | string) =>
-  dayjs(msec).format('YYYY-M-D HH:mm:ss')
--- src/utils/goto.ts
+++
@@ -1,7 +0,0 @@
-const a = document.createElement('a')
-a.target = '_blank'
-export const goto = (url: string) => {
-  a.href = url
-  a.click()
-  a.href = ''
-}
