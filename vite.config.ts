1575377fef63abeae14583d0c151e04223ce4b2e
朱博超 <1060007045@qq.com>
2022-08-07 04:00:08 +0800

    修改类型声明

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
-console.log(__dirname)
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
   },
-  plugins: [vue()],
+  plugins: [ dts({ insertTypesEntry: true}),vue()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/libgif.ts'),
       name: 'libgif',
-      fileName: (format) => `libgif.${format}.js`
+      formats: ['es'],
+      fileName: 'libgif'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'sass'],
+      external: ['vue'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
