7a44e3d7fd23d98fb3f43408932c6df90fe62293
朱博超 <1060007045@qq.com>
2022-09-16 00:13:00 +0800

    修改示例

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,5 @@
-import useDemo from './useDemo' 
-import libDev from '@/../lib/libgif'
-import libExample from '@zaqmjuop/libgif'
- 
-import { GIFS } from './metaData'
+import useDemo from './useDemo'
 
 
-const rub = useDemo(libDev, document.getElementById('dev')!)
-// useDemo(libExample, document.getElementById('example')!)
 
-// setTimeout(() => {
-//   rub.load_url(GIFS[0])
-//   setTimeout(() => {
-//     rub.load_url(GIFS[3])
-//     setTimeout(() => {
-//       rub.load_url(GIFS[1])
-//     }, 2000);
-//   }, 2000);
-// }, 2000);
+useDemo()
--- src/useDemo.ts
+++ src/useDemo.ts
@@ -1,21 +1,37 @@
- 
+
 import { GIFS } from './metaData'
-export default <T extends any>(lib: T, root: HTMLElement) => {
-  const src = GIFS[0]
+import libDev from '@/../lib/libgif'
+export default () => {
+  const root = document.getElementById('useDemo')!
+  const src = GIFS[1]
   const id = Math.random().toString().slice(2)
-  root.innerHTML = `
-  <div
-  id="${id}"
-  src="${src}"
-  rel:animated_src="${src}"
-  width="300"
-  height="300"
-  rel:auto_play="1"
-  rel:rubbable="1"
-  />
+
+
+  root.innerHTML = ` 
+    <div style="margin: 8px 0">
+     ${GIFS.map((url, index) => `<button url="${url}">GIF${index}</button>`).join(' ')}
+    </div>
+    <div
+    id="${id}"
+    src="${src}"
+    rel:animated_src="${src}"
+    width="300"
+    height="300"
+    rel:auto_play="1"
+    rel:rubbable="1"
+    /> 
   `
-  const container = document.getElementById(id) 
-  const rub = (lib as any)({ gif: container! })
+  const container = document.getElementById(id)!
+  const rub = (libDev)({ gif: container })
   rub.load()
+
+  root.addEventListener('click', (e) => {
+    if (e.target instanceof HTMLElement) {
+      const targetUrl = e.target.getAttribute('url')
+      if (targetUrl) {
+        rub.load_url(targetUrl)
+      }
+    }
+  })
   return rub
 }
