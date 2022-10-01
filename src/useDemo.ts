68479bf9dfbcf8eea629d27cb0353dafacfcb518
朱博超 <1060007045@qq.com>
2022-10-01 15:02:14 +0800

    修改 gitpage

--- src/useDemo.ts
+++ src/useDemo.ts
@@ -23,7 +23,7 @@ export default () => {
     /> 
   `
   const container = document.getElementById(id)!
-  const rub = (libDev)({ gif: container })
+  const rub = (libgif)({ gif: container })
   rub.load()
 
   root.addEventListener('click', (e) => {
