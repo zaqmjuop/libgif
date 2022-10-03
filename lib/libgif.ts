e845a026c480891a66508df756a49b0b4d300d34
朱博超 <1060007045@qq.com>
2022-10-03 18:22:11 +0800

    注释

--- lib/libgif.ts
+++ lib/libgif.ts
@@ -20,7 +20,6 @@ const libgif = (opts: Options) => {
   const EMITS = ['loadstart', 'load', 'progress', 'error', 'finish'] as const
   const emitter = new Emitter<typeof EMITS>()
   const options: Options = Object.assign({}, opts)
-
   const gif = options.gif
   // global func
   // global func
@@ -113,30 +112,30 @@ const libgif = (opts: Options) => {
   }
   load()
 
-  const controls2 = {
-    get playing() {
-      return player.playing
-    },
-    get sourceWidth() {
-      return player.header.width
-    },
-    get sourceHeight() {
-      return player.header.height
-    },
-    currentSrc: '', // 只读 地址
-    defaultPlaybackRate: 1, // 默认播放速度
-    playbackRate: 1, // 播放速度
-    duration: 1, // 总时长 只读
-    ended: 1, // 播放完毕 只读
-    error: 1, // 错误 只读
-    initialTime: 1, //初始播放位置（以秒为单位）。 只读
-    loop: 1, //
-    mediaGroup: [], // 连播
-    paused: true, // 指示媒体元素是否被暂停 只读
-    played: [], // 播放过的帧 只读
-    preload: 'auto',
-    readyState: 1 // 准备状态
-  }
+  // const controls2 = {
+  //   get playing() {
+  //     return player.playing
+  //   },
+  //   get sourceWidth() {
+  //     return player.header.width
+  //   },
+  //   get sourceHeight() {
+  //     return player.header.height
+  //   },
+  //   currentSrc: '', // 只读 地址
+  //   defaultPlaybackRate: 1, // 默认播放速度
+  //   playbackRate: 1, // 播放速度
+  //   duration: 1, // 总时长 只读
+  //   ended: 1, // 播放完毕 只读
+  //   error: 1, // 错误 只读
+  //   initialTime: 1, //初始播放位置（以秒为单位）。 只读
+  //   loop: 1, //
+  //   mediaGroup: [], // 连播
+  //   paused: true, // 指示媒体元素是否被暂停 只读
+  //   played: [], // 播放过的帧 只读
+  //   preload: 'auto',
+  //   readyState: 1 // 准备状态
+  // }
 
   const controller = {
     player,
