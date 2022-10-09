c54f4c6581713c78289f70190bcc1b3cf243d03d
朱博超 <1060007045@qq.com>
2022-10-09 21:18:54 +0800

    feat: requireFoolWebpack ts

--- lib/workerpool/requireFoolWebpack.ts
+++ lib/workerpool/requireFoolWebpack.ts
@@ -1,8 +1,7 @@
 // source of inspiration: https://github.com/sindresorhus/require-fool-webpack
-var requireFoolWebpack = eval(
-    'typeof require !== \'undefined\' ' +
+const requireFoolWebpack = eval(
+  "typeof require !== 'undefined' " +
     '? require ' +
     ': function (module) { throw new Error(\'Module " + module + " not found.\') }'
-);
-
-module.exports = requireFoolWebpack;
+)
+export default requireFoolWebpack
