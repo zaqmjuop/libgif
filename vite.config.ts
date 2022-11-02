37c0b1b6a9b8643f03ef6f2a97d10eabdebaf18d
朱博超 <1060007045@qq.com>
2022-11-02 23:00:02 +0800

    修改打包文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,31 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import * as fs from 'fs'
+
+const NAME = 'libgif'
+const ASSETS_DIR = 'assets'
+const OUT_DIR = 'dist'
+const ASSETS_DIR_REGEXP = new RegExp(`${NAME}/${ASSETS_DIR}`, 'g')
+const writeOutDir = (str: string) =>
+  str.replace(ASSETS_DIR_REGEXP, `${NAME}/dist/${ASSETS_DIR}`)
+
+function workerResolverPlugin() {
+  return {
+    name: 'worker-resolver',
+    writeBundle(options, bundle: { [fileName: string]: any }) {
+      const dir = options.dir
+      Object.keys(bundle).forEach((fileName) => {
+        const info = bundle[fileName]
+        if (fileName.endsWith('mjs') && info.type === 'chunk') {
+          const filePath = path.resolve(dir, fileName)
+          const buffer = fs.readFileSync(filePath)
+          const code = buffer.toString('utf8', 0)
+          fs.writeFileSync(filePath, writeOutDir(code))
+        }
+      })
+    }
+  }
+}
 
 let buildType = 'lib'
 
@@ -10,7 +36,7 @@ export default defineConfig({
   server: {
     port: 3000,
     watch: {
-      ignored: ['!**/node_modules/@zaqmjuop/libgif/dist/libgif.mjs']
+      ignored: [`!**/node_modules/@zaqmjuop/${NAME}/${OUT_DIR}/libgif.mjs`]
     }
   },
   optimizeDeps: {
@@ -18,7 +44,7 @@ export default defineConfig({
   },
   plugins: [],
   build: {
-    outDir: buildType === 'lib' ? 'dist' : 'docs',
+    outDir: buildType === 'lib' ? OUT_DIR : 'docs',
     lib:
       buildType === 'lib'
         ? {
@@ -27,7 +53,10 @@ export default defineConfig({
             formats: ['es'],
             fileName: 'libgif'
           }
-        : void 0
+        : void 0,
+    rollupOptions: {
+      plugins: [workerResolverPlugin()]
+    }
   },
   base: 'libgif', // git-page 基础路径
   preview: {
